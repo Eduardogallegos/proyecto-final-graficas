83de41eb188a2348c8bec1f73d06f882fb40c5d9
JavierZavala17 <javier_z_t@hotmail.com>
2021-06-09 17:44:31 -0500

    Fisica v1.0.1

--- js/character.js
+++ js/character.js
@@ -3,6 +3,7 @@ import { PointerLockControls } from "../libs/three.js/r125/controls/PointerLockC
 import { FBXLoader } from "../libs/three.js/r125/loaders/FBXLoader.js";
 
 class MainCharacter {
+
   camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
@@ -17,39 +18,39 @@ class MainCharacter {
     15
   );
 
-  frontRaycaster = new THREE.Raycaster(
+  raycasterWalls = new THREE.Raycaster(
     new THREE.Vector3(),
     //this.camera,
     //this.frontRaycaster.ray.setFromCamera(this.scene, this.camera), //CAMARA + DONDE VE LA CAMARA raycater.ray.setcamera
     // Entender que tenemos que hay que mandarle si uso setCamera(mosue,camera)
     // crosspoint
-    new THREE.Vector3(0,0,-1),
-    0,
-    5
-  );
-
-  backRaycaster = new THREE.Raycaster(
     new THREE.Vector3(),
-    new THREE.Vector3(0,0,1),
     0,
-    5
+    this.wallDistance + 0.1
   );
 
-  rightRaycaster = new THREE.Raycaster(
-    new THREE.Vector3(),
-    new THREE.Vector3(1,0,0),
-    0,
-    5
-  );
+  // backRaycaster = new THREE.Raycaster(
+  //   new THREE.Vector3(),
+  //   new THREE.Vector3(0,0,1),
+  //   0,
+  //   5
+  // );
 
-  leftRaycaster = new THREE.Raycaster(
-    new THREE.Vector3(),
-    new THREE.Vector3(-1,0,0),
-    0,
-    5
-  );
+  // rightRaycaster = new THREE.Raycaster(
+  //   new THREE.Vector3(),
+  //   new THREE.Vector3(1,0,0),
+  //   0,
+  //   5
+  // );
 
-  direction = new THREE.Vector3();
+  // leftRaycaster = new THREE.Raycaster(
+  //   new THREE.Vector3(),
+  //   new THREE.Vector3(-1,0,0),
+  //   0,
+  //   5
+  // );
+
+  direction = new THREE.Vector3(0,0,0); //directionRay
   velocity = new THREE.Vector3();
   controls = new PointerLockControls(this.camera, document.body);
   moveForward = false;
@@ -57,6 +58,8 @@ class MainCharacter {
   moveLeft = false;
   moveRight = false;
   canJump = false;
+  playerSpeed = 5;
+  wallDistance = 0.1;
   prevTime = Date.now();
   characterGroup =  new THREE.Object3D();
 
@@ -78,69 +81,81 @@ class MainCharacter {
   }
 
   update(objects) {
-    
-    this.downRaycaster.ray.origin.copy(this.controls.getObject().position);
-    this.downRaycaster.ray.origin.y -= 50;
-    let downintersections = this.downRaycaster.intersectObjects(objects);
-    let onObject = downintersections.length > 0;
 
-    this.frontRaycaster.ray.origin.copy(this.controls.getObject().position);
-    let frontintersections = this.frontRaycaster.intersectObjects(objects);
-    let frontBump = frontintersections.length > 0;
-
-    this.backRaycaster.ray.origin.copy(this.controls.getObject().position);
-    let backintersections = this.backRaycaster.intersectObjects(objects);
-    let backBump = backintersections.length > 0;
-
-    this.rightRaycaster.ray.origin.copy(this.controls.getObject().position);
-    let rightintersections = this.rightRaycaster.intersectObjects(objects);
-    let rightBump = rightintersections.length > 0;
-
-    this.leftRaycaster.ray.origin.copy(this.controls.getObject().position);
-    let leftintersections = this.leftRaycaster.intersectObjects(objects);
-    let leftBump = leftintersections.length > 0;
-
-    
     let time = Date.now();
     let delta = (time - this.prevTime) / 800;
 
-    this.velocity.x -= this.velocity.x * 1.0 * delta;
-    this.velocity.z -= this.velocity.z * 1.0 * delta;
-    this.velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-    this.direction.z = Number(this.moveForward) - Number(this.moveBackward);
-    this.direction.x = Number(this.moveRight) - Number(this.moveLeft);
+    // this.velocity.x -= this.velocity.x * this.playerSpeed * delta;
+    // this.velocity.z -= this.velocity.z * this.playerSpeed * delta;
+    this.velocity.y -= 10 * 100.0 * delta; // 100.0 = mass
+    //this.direction.z = Number(this.moveForward) - Number(this.moveBackward);
+    //this.direction.x = Number(this.moveRight) - Number(this.moveLeft);
     this.direction.normalize(); // this ensures consistent movements in all directions
+    
+    this.downRaycaster.ray.origin.copy(this.controls.getObject().position);
+    this.downRaycaster.ray.origin.y -= 40;
+    let downintersections = this.downRaycaster.intersectObjects(objects);
 
-    //LOGICA PARA NO ATRAVESAR COSAS
-    if (this.moveForward || this.moveBackward)
-      this.velocity.z -= this.direction.z * 400.0 * delta;
+    
+    let dirCopy = new THREE.Vector3().copy(this.direction);
+    // console.log(dirCopy);
+    let vectorDir = dirCopy.applyMatrix4(new THREE.Matrix4().extractRotation(this.controls.getObject().matrix)).normalize();
+    //console.log (vectorDir);
+    
+    this.raycasterWalls.ray.origin.copy(this.controls.getObject().position);
+    this.raycasterWalls.ray.direction.copy(vectorDir);
+    let intersectionWalls = this.raycasterWalls.intersectObjects(objects);
 
-    if (this.moveLeft || this.moveRight)
-      this.velocity.x -= this.direction.x * 400.0 * delta;
+    if ( this.moveForward && !(dirCopy.z!== 0 && intersectionWalls.length >= 1)) this.controls.getObject().translateZ(-this.playerSpeed * delta * 100);
+    if ( this.moveBackward && !(dirCopy.z!==0 && intersectionWalls.length >= 1)) this.controls.getObject().translateZ(this.playerSpeed * delta * 100);
+    if ( this.moveLeft && !(dirCopy.x!==0 && intersectionWalls.length >= 1)) this.controls.getObject().translateX(-this.playerSpeed * delta * 100);
+    if ( this.moveRight && !(dirCopy.x!==0 && intersectionWalls.length >= 1)) this.controls.getObject().translateX(this.playerSpeed * delta * 100);
 
-    if (onObject === true) {
+
+
+    // let frontBump = frontintersections.length > 0;
+    // this.backRaycaster.ray.origin.copy(this.controls.getObject().position);
+    // let backintersections = this.backRaycaster.intersectObjects(objects);
+    // let backBump = backintersections.length > 0;
+
+    // this.rightRaycaster.ray.origin.copy(this.controls.getObject().position);
+    // let rightintersections = this.rightRaycaster.intersectObjects(objects);
+    // let rightBump = rightintersections.length > 0;
+
+    // this.leftRaycaster.ray.origin.copy(this.controls.getObject().position);
+    // let leftintersections = this.leftRaycaster.intersectObjects(objects);
+    // let leftBump = leftintersections.length > 0;
+
+    
+    // MOVIMIENTO
+    // if (this.moveForward || this.moveBackward)
+    //   this.velocity.z -= this.direction.z * 800.0 * delta;
+
+    // if (this.moveLeft || this.moveRight)
+    //   this.velocity.x -= this.direction.x * 800.0 * delta;
+
+    // SALTO
+    if (downintersections.length > 0 === true) {
       this.velocity.y = Math.max(0, this.velocity.y);
       this.canJump = true;
     }
 
-    if (frontBump === true){
-      this.velocity.z = Math.max(0, this.velocity.z);
-    }
+    //LOGICA PARA NO ATRAVESAR COSAS
+    // if (frontBump === true){
+    //   this.velocity.z = Math.max(0, this.velocity.z);
+    // }
+    // if (backBump === true){
+    //   this.velocity.z = Math.min(0, this.velocity.z);
+    // }
+    // if (rightBump === true){
+    //   this.velocity.x = Math.max(0, this.velocity.x);
+    // }
+    // if (leftBump === true){
+    //   this.velocity.x = Math.min(0, this.velocity.x);
+    // }
 
-    if (backBump === true){
-      this.velocity.z = Math.min(0, this.velocity.z);
-    }
-
-    if (rightBump === true){
-      this.velocity.x = Math.max(0, this.velocity.x);
-    }
-
-    if (leftBump === true){
-      this.velocity.x = Math.min(0, this.velocity.x);
-    }
-
-    this.controls.moveRight(-this.velocity.x * delta);
-    this.controls.moveForward(-this.velocity.z * delta);
+    // this.controls.moveRight(-this.velocity.x * delta);
+    // this.controls.moveForward(-this.velocity.z * delta);
 
     this.controls.getObject().position.y += this.velocity.y * delta; // new behavior
 
@@ -257,15 +272,19 @@ class MainCharacter {
     switch (newDirection) {
       case 0:
         this.moveForward = true;
+        this.direction.z = -this.wallDistance;
         break;
       case 1:
         this.moveBackward = true;
+        this.direction.z = this.wallDistance;
         break;
       case 2:
         this.moveLeft = true;
+        this.direction.x = -this.wallDistance;
         break;
       case 3:
         this.moveRight = true;
+        this.direction.x = this.wallDistance;
         break;
     }
   }
@@ -273,15 +292,19 @@ class MainCharacter {
     switch (newDirection) {
       case 0:
         this.moveForward = false;
+        this.direction.z = 0;
         break;
       case 1:
         this.moveBackward = false;
+        this.direction.z = 0;
         break;
       case 2:
         this.moveLeft = false;
+        this.direction.x = 0;
         break;
       case 3:
         this.moveRight = false;
+        this.direction.x = 0;
         break;
     }
   }
